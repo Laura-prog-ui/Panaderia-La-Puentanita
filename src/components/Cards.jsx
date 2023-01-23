363b4b200183fbbb98d84efdd1dab516867e8110
Laura-prog-ui <duarteriveralauraestefania0@gmail.com>
2023-01-23 13:51:59 -0500

    Commit

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,7 +3,7 @@ import Card from './Card';
 
 function Cards() {
     return(
-        <div className='container d-flex justify-content-center'>
+        <div className='container d-flex justify-content-center aling h-100'>
             <div className='row'>
                 <div className='col-md-4'>
                     <Card/>
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './Styles/tailwind.css';
-import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
